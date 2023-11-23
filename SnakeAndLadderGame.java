84bd20648275bf3129ed80305830786ff2be1a5d
vivektangudu2024 <tanguduvivek2003.com>
2023-11-23 07:24:37 +0530

    added UC3_playeroption

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -13,16 +13,57 @@ public class SnakeAndLadderGame {
         int dieRoll = rollDie();
 
         System.out.println("Player rolled a " + dieRoll);
+
+        // Check the option: No play, Ladder, or Snake
+        int option = checkOption();
+
+        // Update player position based on the option
+        playerPosition = updatePosition(playerPosition, dieRoll, option);
+
+        // Display the current position
+        System.out.println("Player's current position: " + playerPosition);
     }
 
     /*
      * @Desc : Simulating dice roll
      * @Params: none
-     * @returns: value btwn 1 to 6
+     * @returns: value between 1 to 6
      */
     private static int rollDie() {
         Random random = new Random();
         return random.nextInt(6) + 1;
+    }
+
+    /*
+     * @Desc : check the option: No play, Ladder, or Snake
+     * @Params: none
+     * @returns: 0 for No play, 1 for Ladder, 2 for Snake
+     */
+    private static int checkOption() {
+        Random random = new Random();
+        return random.nextInt(3);
+    }
+
+    /*
+     * @Desc : update player position based on the die roll and option
+     * @Params: position, dieRoll, option
+     * @returns: updated player position
+     */
+    private static int updatePosition(int position, int dieRoll, int option) {
+        switch (option) {
+            case 0: // No play
+                System.out.println("No play! Player stays in the current position.");
+                break;
+            case 1: // Ladder
+                System.out.println("Ladder! Player moves ahead by " + dieRoll + " positions.");
+                position += dieRoll;
+                break;
+            case 2: // Snake
+                System.out.println("Snake! Player moves behind by " + dieRoll + " positions.");
+                position = Math.max(0, position - dieRoll);
+                break;
+        }
+        return position;
     }
 
 }
