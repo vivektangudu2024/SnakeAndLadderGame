21813bbb62c1b039ff156ba5800fe03b0af216a5
vivektangudu2024 <tanguduvivek2003.com>
2023-11-23 07:16:56 +0530

    Added UC2 Diceroll

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -9,6 +9,20 @@ public class SnakeAndLadderGame {
         // Initialize player position
         int playerPosition = 0;
 
+        // Roll the dice
+        int dieRoll = rollDie();
+
+        System.out.println("Player rolled a " + dieRoll);
+    }
+
+    /*
+     * @Desc : Simulating dice roll
+     * @Params: none
+     * @returns: value btwn 1 to 6
+     */
+    private static int rollDie() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
     }
 
 }
