45a0c1fdc406c86f2de214e4f6d3e2846f8f9f1e
vivektangudu2024 <tanguduvivek2003.com>
2023-11-23 07:04:27 +0530

    Added UC1

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,14 @@
+package com.day3.snakeAndLadder;
+
+import java.util.*;
+
+public class SnakeAndLadderGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game!");
+
+        // Initialize player position
+        int playerPosition = 0;
+
+    }
+
+}
